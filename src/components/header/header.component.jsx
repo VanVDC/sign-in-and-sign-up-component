91b2e74ef1d9fb71243bde4a402d3e9bfa93202c
VanVDC <vanbvy@gmail.com>
2020-01-17 22:21:00 -0500

    fixed the header

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -24,7 +24,7 @@ const Header = ({ currentUser }) => (
           SIGN OUT
         </div>
       ) : (
-        <Link className="option" ro="/signin">
+        <Link className="option" to="/signin">
           {" "}
           SIGN IN
         </Link>
